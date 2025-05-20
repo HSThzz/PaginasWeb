0ef0e2feb22a237f7e257c5212eca26ce0b06b2f
Thiago Esmerio <thiagoesmerio123@gmail.com>
2025-05-20 19:13:56 -0300

    calculadora usando react

--- CalculadoraReact/src/App.jsx
+++ CalculadoraReact/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+//
 function App() {
   //renderizaçao de pagina
   const [resultado, setResultado] = useState("")
